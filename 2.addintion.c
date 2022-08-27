8929b82e154785fc4fbd1896e39b7a3e9affde3b
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-08-27 08:55:35 -0900

    Add files via upload

---
+++ 2.addintion.c
@@ -0,0 +1,8 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two digit:");
+	scanf("%d%d",&a,&b);
+	printf("Addition is: %d",a+b);
+}
---
+++ 3.calculator.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+ int main()
+{
+    int a,b;
+    float result;
+    char ch;
+     printf("Enter your Expression:-");
+     scanf("%d%c%d",&a,&ch,&b);
+    result=0;
+    switch(ch)    
+    {
+        case '+':result=a+b;
+            break;
+        case '-':result=a-b;
+            break;
+        case '*':result=a*b;
+            break;
+        case '/':result=(float)a/(float)b;
+            break;
+        case '%':result=a%b;
+            break;
+        default:printf("Invalid operation...\n");
+    }
+    printf("Result: %d %c %d = %f\n",a,ch,b,result);
+    return 0;
+}
