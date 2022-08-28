3932823a3da99706e0eb7356901db67411e2986b
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-08-28 09:06:49 -0900

    Add files via upload

---
+++ 4.number_pattern.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i,a=1;
+    for(i=1;i<=28;i++)
+    {
+        if(i==8 || i==15 || i==22)
+        {
+            printf("\n\n");
+        }
+        if(i>=1 && i<=3 || i>=5 && i<=7 || i==8 || i==9 ||i==13 || i==14 ||i==15 || i==21)
+        {
+            printf("    ");
+        }
+        else
+        {
+            printf("%4d",a);
+            a++;
+        }
+    }
+}
