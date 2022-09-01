873437f771c65b6def9c672341efb0872ea9963b
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-01 09:16:05 -0900

    Add files via upload

---
+++ 8.number_pattern.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i,j,k,sp=4,a=15;
+    for(i=1;i<=5;i++)
+    {
+        for(k=1;k<=sp;k++)
+        {
+            printf("\t");
+        }
+        sp--;
+        for(j=1;j<=i;j++)
+        {
+             printf("%d\t",a);
+             a--;
+        }
+        printf("\n");
+     }
+ }
