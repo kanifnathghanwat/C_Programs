3106ab5db71a899f55ae0cb66df7eeb13937f3de
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-02 08:49:48 -0900

    Add files via upload

---
+++ 9.star_pattern.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i,j,k,sp=3;
+    for(i=1;i<=3;i++)
+    {
+        for(k=1;k<=sp;k++)
+        {
+            printf(" ");
+         }
+        sp--;
+        {
+            for(j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+         }
+         printf("\n");
+     }
+  /*  sp=1;
+    for(i=1;i<=2;i++)
+    {
+        for(k=0;k<=sp;k++)
+        {
+            printf(" ");
+        }
+        sp++;
+            for(j=2;j>=i;j--)
+            {
+                printf("*");
+            }
+            printf("\n");
+        
+    }*/
+}
