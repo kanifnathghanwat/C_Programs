721d3b788f921b347292249b63b3dac08bcdbd96
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-23 04:49:52 -0900

    Add files via upload

---
+++ 17.table_using_for_loop.c
@@ -0,0 +1,33 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=10;i++)
+    {
+        for(j=1;j<=10;j++)
+        {
+            printf("%4d",i*j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    for(i=11;i<=20;i++)
+    {
+        for(j=1;j<=10;j++)
+        {
+            printf("%4d",i*j);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    for(i=21;i<=30;i++)
+    {
+        for(j=1;j<=10;j++)
+        {
+            printf("%4d",i*j);
+        }
+        printf("\n");
+    }
+}
