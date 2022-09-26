69329040c9199f1443053eb131c47c560c4c57b9
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-26 07:17:11 -0900

    Add files via upload

---
+++ 19.fibonacci_series.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main() 
+{
+	int i, num, n1=0, n2=1, n3;
+	printf("Enter the number of elements: ") ;
+	scanf("%d", &num);
+	printf("%3d %3d", n1, n2);
+	for(i=2;i<num;i++) 
+	{
+		n3 = n1 + n2;
+		printf("%3d", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+}
