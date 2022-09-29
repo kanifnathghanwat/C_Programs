ebee5ff004377758458f71d878aad9a7693491bc
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-29 03:27:07 -0900

    Add files via upload

---
+++ 21.prime_number.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main()
+{
+    int i=2,no,k=0;
+    printf("Enter a no: ");
+    scanf("%d",&no);
+    while(i<=no/2)
+    {
+        if(no%i==0)
+        {
+            k++;
+        }
+        i++;
+    }
+    if(k==0)
+    {
+        printf("%d is Prime number...", no);
+    }
+    else
+     {
+        printf("%d is not Prime number...", no);
+     }
+}
