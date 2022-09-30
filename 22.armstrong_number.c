43694fffb94dd85481438f8dcc26000b7bda73f1
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-30 08:39:52 -0900

    Add files via upload

---
+++ 22.armstrong_number.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main() 
+{
+	int n, r, temp, sum=0;
+	printf("Enter the number: ") ;
+	scanf("%d", &n) ;
+	temp=n;
+	while (n>0) 
+	{
+		r = n % 10;
+		sum = sum + (r * r * r);
+		n = n / 10;
+	}
+	if(temp == sum) 
+	    printf("%d is Armstrong number",temp);
+	else
+		printf("%d is Not Armstrong number",temp);
+}
