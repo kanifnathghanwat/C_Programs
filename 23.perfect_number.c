75184b3fba352b6fc83bf4bf680e18c672b16362
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-02 02:07:55 -0900

    Add files via upload

---
+++ 23.perfect_number.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main() 
+{
+	int no, rem, i, sum=0;
+	printf("Enter a number: ");
+	scanf("%d",&no);
+	for(i=1;i<=no/2;i++) 
+	{
+		rem = no % i;
+		if(rem == 0) 
+		   sum = sum + i;
+	}
+	if(sum == no) 
+	   printf("%d is a perfect number",no);
+    else
+	   printf("%d is not a perfect number",no);
+}
