65703870b0d60ecba2c0773f1e9e55246e1dccb8
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-04 07:40:51 -0900

    Add files via upload

---
+++ 24.factorial.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+int main()
+{
+    int fact=1,i,no;
+    printf("Enter a number:");
+    scanf("%d",&no);
+    for(i=1;i<=no;i++)
+    {
+        fact=fact*i;
+    }
+    printf("Factorial of %d is:- %d",no,fact);
+}
