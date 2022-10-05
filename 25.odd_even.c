c9ed61e26393c476c622e5e56dfa3bdcc82141a8
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-05 09:16:59 -0900

    Add files via upload

---
+++ 25.odd_even.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main() 
+{
+	int i;
+	printf("Enter a number: ");
+	scanf("%d", &i);
+	if((i & 1)==1) 
+	   printf("%d is odd",i);
+	if((i & 1)==0) 
+	   printf("%d is even",i);
+}
