41abc736b17e6ce23e999f3fe62f184355deacd0
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-06 03:58:34 -0900

    Add files via upload

---
+++ 26.even_odd_no.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+int main()
+{
+    int a[100],i;
+    for(i=1;i<=100;i++)
+    {
+        a[i]=i;
+    }
+    printf(" 1 to 100 EVEN numbers\n");
+    for(i=1;i<=100;i++)
+    if(a[i]%2==0)
+    {
+       printf("\n\n %d  is even",a[i]);
+     }
+    
+    printf(" 1 to 100 ODD numbers\n");
+    for(i=1;i<=100;i++)
+    if(a[i]%2!=0)
+     {
+          printf("\n\n %d is odd",a[i]);
+     }
+}
